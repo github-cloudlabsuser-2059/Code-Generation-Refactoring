d7a70c2835af3655d6cb280f839e1a45dcd3166e
Matthew <github_cloudlabsuser_2059@cloudlabsaiuser.com>
2025-05-29 17:49:25 +0000

    add ace and face cards

--- card_draw.py
+++ card_draw.py
@@ -4,6 +4,10 @@
 import itertools
 import random
 
+def card_name(value):
+    names = {1: "Ace", 11: "Jack", 12: "Queen", 13: "King"}
+    return names.get(value, str(value))
+
 # make a deck of cards
 deck = list(itertools.product(range(1, 14), ['Spade', 'Heart', 'Diamond', 'Club']))
 
@@ -13,4 +17,4 @@ random.shuffle(deck)
 # draw five cards
 print("You got:")
 for i in range(5):
-    print(deck[i][0], "of", deck[i][1])
+    print(f"{card_name(deck[i][0])} of {deck[i][1]}")
